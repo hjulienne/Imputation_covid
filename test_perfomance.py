247856d3671f0fefbba0d448597ca73ff9c02154
hjulienne <hanna.julienne@gmail.com>
2020-12-08 15:43:57 +0100

    code snippet

--- test_perfomance.py
+++ test_perfomance.py
@@ -1,23 +1,28 @@
 import raiss
 import pandas as pd
+import sys
+
+base_dir = sys.argv[1]
+code_GWAS = sys.argv[2]
+print(sys.argv)
+# fi_"{0}/z_score_chr/z_{1}_chr22.txt".format(base_dir, code_GWAS)
+# Zscore = pd.read_csv(, sep="\t", index_col=0)
+#
+# Zscore.head()
+# Z_masked = raiss.imputation_R2.generated_test_data(Zscore,20000)
+# len(Z_masked[1])
+#
+# f_out_masked = "{0}/z_score_masked/z_{1}_chr22.txt".format(base_dir, code_GWAS)
+# Z_masked[0].to_csv(f_out_masked, sep="\t")
 
 
-Zscore = pd.read_csv("/home/hjulienn/Project/COVID-19/Imputation_HGI/z_score_chr/z_COVID_FINNGEN_chr22.txt", sep="\t", index_col=0)
-
-Zscore.head()
-Z_masked = raiss.imputation_R2.generated_test_data(Zscore,20000)
-len(Z_masked[1])
-
-Z_masked[0].to_csv("/home/hjulienn/Project/COVID-19/Imputation_HGI/z_score_masked/z_COVID_FINNGEN_chr22.txt", sep="\t")
-
-
-perf_results = raiss.imputation_R2.grid_search("/home/hjulienn/Project/COVID-19/Imputation_HGI/z_score_chr/",
-                                "/home/hjulienn/Project/COVID-19/Imputation_HGI/z_score_masked/",
-                                "/home/hjulienn/Project/COVID-19/Imputation_HGI/z_score_imputed/",
-                                "/home/hjulienn/Project/COVID-19/Imputation_HGI/ref_panel/",
-                                "/home/hjulienn/Project/COVID-19/Imputation_HGI/ld/",
-                                "COVID_FINNGEN", eigen_ratio_grid =[0.1, 0.01, 0.005, 0.001], N_to_mask=20000,
+perf_results = raiss.imputation_R2.grid_search("{0}/z_score_chr/".format(base_dir),
+                                "{0}/z_score_masked/".format(base_dir),
+                                "{0}/z_score_imputed/".format(base_dir),
+                                "{0}/ref_panel/".format(base_dir),
+                                "{0}/ld/".format(base_dir),
+                                code_GWAS, eigen_ratio_grid =[0.1, 0.01, 0.005, 0.001], N_to_mask=10000,
                                 ref_panel_suffix = ".bim",
                                 ld_type="scipy")
 
-perf_results.to_csv("/home/hjulienn/Project/COVID-19/Imputation_HGI/imputation_test/performance_COVID_FINNGEN.csv")
+perf_results.to_csv("{0}/imputation_performance/performance_{1}.csv".format(base_dir, code_GWAS))
